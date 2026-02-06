cb5113bbb261fb5a46e3050a35f5a0542390c98f
SyedArhanRizvi <arhanrizvi9@gmail.com>
2026-02-06 15:41:51 +0530

    add google sheet and add smooth scroll code

--- script.js
+++ script.js
@@ -231,3 +231,15 @@
             });
     });
 })();
+
+
+// Initialize Lenis
+const lenis = new Lenis();
+
+// Use requestAnimationFrame to continuously update the scroll
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
