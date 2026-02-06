4f0072eb564bc8f1d0b519ecee3880cea0511147
varun <varunmathur817@gamil.com>
2026-02-06 17:56:59 +0530

    remove the smooth scrolling

--- script.js
+++ script.js
@@ -233,13 +233,3 @@
 })();
 
 
-// Initialize Lenis
-const lenis = new Lenis();
-
-// Use requestAnimationFrame to continuously update the scroll
-function raf(time) {
-  lenis.raf(time);
-  requestAnimationFrame(raf);
-}
-
-requestAnimationFrame(raf);
